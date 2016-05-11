23861a577b80532df18a4c765d8474a190c434c2
George Edwards <g.edwards@gas-sense.co.uk>
2016-05-11 21:58:24 +0100

    Responded to comments

--- Auth/auth-common.ts
+++ Auth/auth-common.ts
@@ -2,7 +2,6 @@ import * as definition from "nativescript-azure-mobile-apps/user";
 
 export abstract class MobileServiceUser implements definition.MobileServiceUser {
     
-    abstract login(provider: string): Promise<definition.MobileServiceUser>;
     abstract setUserId(userId: string): void;
     abstract setAuthenticationToken(authenticationToken: string): void;
     abstract getUserId(): string;
