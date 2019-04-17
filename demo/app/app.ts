c97aac4372d339cf0d41fdfe5c8f80e74657c219
Peter Staev <peter@tangrasoft.com>
2019-04-17 14:26:14 +0300

    change depreciated push plugin

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -2,6 +2,10 @@ import "./bundle-config";
 
 import * as application from "application";
 
+// This needs to be first as we override the whole app delegate!
+// Needed only if you are using authentication. 
 require("nativescript-azure-mobile-apps/client").MobileServiceClient.configureClientAuthAppDelegate();
+
+require("nativescript-plugin-firebase");
 
 application.start({ moduleName: "main-page" });
--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -6,7 +6,8 @@ import { ActivityIndicator } from "ui/activity-indicator";
 import { Page } from "ui/page";
 import { EventData } from "data/observable";
 import * as platform from "platform";
-let pushPlugin = require("nativescript-push-notifications");
+
+import { messaging } from "nativescript-plugin-firebase/messaging";
 
 class TodoItem {
     public id: string;
@@ -19,25 +20,27 @@ let todoItemTable: MobileServiceTable;
 let item: TodoItem;
 let ai: ActivityIndicator;
 
-let pushSettings = {
-    senderID: "271351633466",
-    // Android: Callback to invoke when a new push is received.    
-    notificationCallbackAndroid(message) { 
-        console.log(message);
-    }, 
-    
-    badge: true, // Enable setting badge through Push Notification
-    sound: true, // Enable playing a sound
-    alert: true, // Enable creating a alert
-    // Callback to invoke, when a push is received on iOS
-    notificationCallbackIOS(message) {
-        console.log(message);
-    }
-};
-
 let pushTemplates = {};
-pushTemplates[platform.platformNames.android] = "{\"notification\":{\"text\":\"$(param)\"}}";
-pushTemplates[platform.platformNames.ios] = "{\"aps\":{\"alert\":\"$(param)\"}}";
+pushTemplates[platform.platformNames.android] = JSON.stringify({
+    data: {
+        title: "$(title)",
+        message: "$(message)",
+    },
+    notification: {
+        title: "$(title)",
+        text: "$(message)",
+    },
+});
+pushTemplates[platform.platformNames.ios] = JSON.stringify({
+    aps: {
+        alert: {
+            title: "$(title)",
+            body: "$(message)",
+        },
+        sound: "default",
+        badge: 1,
+    },
+});
 
 export function onNavigatingTo(args: EventData) {
     client = new MobileServiceClient("https://tangrainctest.azurewebsites.net");
@@ -47,19 +50,19 @@ export function onNavigatingTo(args: EventData) {
 
 export function onAddItemTap(args) {
     ai.busy = true;
-    
+
     item = new TodoItem();
     item.text = "NativeScript Rocks";
     item.completed = false;
-    
+
     todoItemTable.insert(item).then((updatedItem) => {
         ai.busy = false;
-        
+
         item = updatedItem;
         dialogs.alert("Item added!");
     }, (e) => {
         ai.busy = false;
-        
+
         console.log("Error adding item!", e);
     });
 }
@@ -69,11 +72,11 @@ export function onUpdateItemTap(args) {
         dialogs.alert("There is no item. Use Add first!");
         return;
     }
-    
+
     ai.busy = true;
-    
+
     item.completed = true;
-    
+
     todoItemTable.update(item).then((updatedItem) => {
         ai.busy = false;
 
@@ -88,7 +91,7 @@ export function onUpdateItemTap(args) {
 
 export function onGetAllItemsTap(args) {
     ai.busy = true;
-    
+
     todoItemTable.read().then((results) => {
         ai.busy = false;
 
@@ -102,7 +105,7 @@ export function onGetAllItemsTap(args) {
 
 export function onGetCompletedItemsTap(args) {
     ai.busy = true;
-    
+
     todoItemTable.where().field("completed").eq(true).read().then((results) => {
         ai.busy = false;
 
@@ -119,9 +122,9 @@ export function onDeleteItemTap(args) {
         dialogs.alert("There is no item. Use Add first!");
         return;
     }
-    
+
     ai.busy = true;
-    
+
     todoItemTable.deleteItem(item).then(() => {
         ai.busy = false;
 
@@ -136,21 +139,21 @@ export function onDeleteItemTap(args) {
 
 export function onLoginTap(args) {
     ai.busy = true;
-    
+
     if (client.loginFromCache()) {
         console.log(`Logged in via cached credentials! UserID: ${client.user.userId}`);
-        
+
         ai.busy = false;
     }
     else {
         client.login(AuthenticationProvider.Google, "x-msauth-tns-azure-sample").then((user) => {
             console.log(`Logged In! UserID: ${user.userId}`);
-            
+
             user.getProviderCredentials().then((result) => {
                 console.log(`Surname: ${result.surname}`);
                 console.log(`Given Name: ${result.givenName}`);
                 console.log(`Name: ${result.name}`);
-            
+
                 ai.busy = false;
             });
         }, (e) => {
@@ -162,43 +165,51 @@ export function onLoginTap(args) {
 }
 
 export function onPushRegisterTap() {
-    pushPlugin.register(pushSettings, (data) => {
-        if (pushPlugin.onMessageReceived) {
-            pushPlugin.onMessageReceived(pushSettings.notificationCallbackAndroid);
+    messaging.registerForPushNotifications({
+        onMessageReceivedCallback: (message) => {
+            console.log(message);
+        },
+        onPushTokenReceivedCallback: (token) => {
+            client.push.register(token)
+                .then(() => { console.log("Azure Register OK!", client.push.installationId); })
+                .catch((e) => { console.error(e); });
         }
-        client.push.register(data)
-            .then(() => console.log("Azure Register OK!", client.push.installationId))
-            .catch((e) => console.log(e));
-    }, (e) => { console.log(e); });
+    });
 }
 
 export function onPushTemplateRegisterTap() {
-    pushPlugin.register(pushSettings, (data) => {
-        if (pushPlugin.onMessageReceived) {
-            pushPlugin.onMessageReceived(pushSettings.notificationCallbackAndroid);
-        }
-        client.push.registerWithTemplate(data, "MyTemplate", pushTemplates[platform.device.os])
-            .then(() => console.log("Azure Register OK!"))
-            .catch((e) => console.log(e));
-    }, (e) => { console.log(e); });
+    messaging.registerForPushNotifications({
+        onMessageReceivedCallback: (message) => {
+            console.log(message);
+        },
+        onPushTokenReceivedCallback: (token) => {
+            client.push.registerWithTemplate(token, "MyTemplate", pushTemplates[platform.device.os])
+                .then(() => { console.log("Azure Register OK!", client.push.installationId); })
+                .catch((e) => { console.error(e); });
+        },
+    });
 }
 
 export function onPushTagsRegisterTap() {
-    pushPlugin.register(pushSettings, (data) => {
-        if (pushPlugin.onMessageReceived) {
-            pushPlugin.onMessageReceived(pushSettings.notificationCallbackAndroid);
-        }
-        client.push.registerWithTags(data, ["test1"])
-            .then(() => console.log("Azure Register OK!"))
-            .catch((e) => console.log(e));
-    }, (e) => { console.log(e); });
+    messaging.registerForPushNotifications({
+        onMessageReceivedCallback: (message) => {
+            console.log(message);
+        },
+        onPushTokenReceivedCallback: (token) => {
+            client.push.registerWithTags(token, ["test1"])
+                .then(() => { console.log("Azure Register OK!", client.push.installationId); })
+                .catch((e) => { console.error(e); });
+        },
+    });
 }
 
 export function onPushUnregisterTap() {
-    pushPlugin.unregister(() => {
-        console.log("Device Unregister OK!");
-        client.push.unregister()
-            .then(() => console.log("Azure Unregister OK!"))
-            .catch((e) => console.log(e));
-    }, (e) => console.log(e), pushSettings);
+    messaging.unregisterForPushNotifications()
+        .then(() => {
+            console.log("Device Unregister OK!");
+            client.push.unregister()
+                .then(() => console.log("Azure Unregister OK!"))
+                .catch((e) => console.log(e));
+        })
+        .catch((e) => { console.error(e); });
 }
